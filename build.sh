20aaa1ac3ea914a17c66742a493a54782e1f93d3
Vaisakh Murali <mvaisakh@statixos.com>
2024-03-04 12:37:02 +0530

    build-script: don't bail out on repo sync
    Change-Id: I670459d468cdcc5bb0ebbd0692a53aaba8716b59
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- build.sh
+++ build.sh
@@ -186,7 +186,7 @@ else
         if [[ ! -z "$target_dirs" ]]; then
             IFS=$'\n' read -r -a target_dir_array <<< "$target_dirs"
             for dir in "${target_dir_array[@]}"; do
-                repo sync -q --no-tags --no-deps --no-remote-sync "$dir" || exit_on_error
+                repo sync -q --no-tags --no-deps --no-remote-sync "$dir"
             done
         else
             echo "Error: Target directories not found in $manifest_file"
